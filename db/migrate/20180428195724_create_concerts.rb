21e87ef99a46526edbf073b25f7d53732aa6f134
Nathan Burgess <nathan@3playmedia.com>
2018-04-28 15:58:39 -0400

    Create concert model

---
+++ db/migrate/20180428195724_create_concerts.rb
@@ -0,0 +1,10 @@
+class CreateConcerts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :concerts do |t|
+      t.datetime :show_date
+      t.references :venue, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
