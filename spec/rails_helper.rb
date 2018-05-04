cdd5b5f11aff48e46a4165333577678c5d54b884
Nathan Burgess <nathan@3playmedia.com>
2018-05-04 15:02:51 -0400

    Add linting to factories in before suite

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -47,6 +47,7 @@ RSpec.configure do |config|
   config.before(:suite) do
    DatabaseCleaner.clean_with(:truncation)
    DatabaseCleaner.strategy = :transaction
+   FactoryBot.lint
   end
 
   config.around(:each) do |example|
